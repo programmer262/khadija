4d6dcf2cbcb933fcc20e8a08eda23887d55fa944
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-10-29 14:56:01 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -94,13 +94,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     # 'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'khadija.urls'
