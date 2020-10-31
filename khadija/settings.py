53e64c20a794194e2798dd2e1294b48d35966d98
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-10-31 15:45:17 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -127,3 +126,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/docs/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/docs')
+STATIC_ROOT = 'static'
