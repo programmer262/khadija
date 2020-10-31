9d33d4df8cb9019ce6d9331c21ff880ae721b124
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-10-31 22:30:06 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -74,11 +74,10 @@ WSGI_APPLICATION = 'khadija.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'db.postgresql_psycopg2',
     }
 }
 
