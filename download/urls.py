7d826c69237e0eefe2ab0a9a06a21b7c90851924
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-11-01 21:48:45 +0100

    Update urls.py

--- download/urls.py
+++ download/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
     path('Cours',views.cour,name='cour'),
     path('exercice/',views.exercice,name='exercice'),
     path('correction/',views.correction,name='correction'),
-  	path('register/', views.registerPage, name="register"),
-	path('', views.loginPage, name="login"),  
-	path('logout/', views.logoutUser, name="logout"),
-	path('classe/', views.classe, name="classe"),
-	path('endlive/', views.live, name="live"),
-
-	]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('register/', views.registerPage, name="register"),
+    path('', views.loginPage, name="login"),  
+    path('logout/', views.logoutUser, name="logout"),
+    path('classe/', views.classe, name="classe"),
+    path('endlive/', views.live, name="live"),
+                ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
