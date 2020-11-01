f1e2015934968b074749f50279fd46ad5b0023c8
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-11-01 17:08:41 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -167,6 +167,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
